01adaa126535101a1903cd9836e1e96f0ff41d71
arquivosaxial-hue <arquivosaxial@gmail.com>
2026-07-21 08:19:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker - DTS
 // Permite que o app funcione offline após a primeira abertura com internet.
-const CACHE = "dts-solution-v20";
+const CACHE = "dts-solution-v22";
 
 // Arquivos do próprio app (mesma origem) para pré-cachear na instalação
 const APP_SHELL = [
